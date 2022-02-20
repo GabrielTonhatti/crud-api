4b83298302a78851886ca31828c327ff1e4e26ee
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-02-20 17:49:02 -0300

    Add exception for entity in use

---
+++ src/main/java/com/gabrieltonhatti/crudapi/domain/exception/EntidadeEmUsoExpcetion.java
@@ -0,0 +1,9 @@
+package com.gabrieltonhatti.crudapi.domain.exception;
+
+public class EntidadeEmUsoExpcetion extends NegocioException {
+
+    public EntidadeEmUsoExpcetion(String message) {
+        super(message);
+    }
+
+}
